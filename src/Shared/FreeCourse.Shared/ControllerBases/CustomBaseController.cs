fa05d097e55a8cee0fc7b46a15c22c21ed772da7
Ali Demir <alidemirytu@gmail.com>
2021-12-14 01:25:08 +0300

    web mvc project added

--- src/Shared/FreeCourse.Shared/ControllerBases/CustomBaseController.cs
+++ src/Shared/FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -5,6 +5,7 @@ namespace FreeCourse.Shared.ControllerBases
 {
     public class CustomBaseController : ControllerBase
     {
+        
         protected IActionResult CreateActionResultInstance<T>(Response<T> response)
         {
             return new ObjectResult(response)
